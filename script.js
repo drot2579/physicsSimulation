99a74260192c81685a9233b71694c7e404e87e03
drot2579 <154900509+drotCode@users.noreply.github.com>
2024-05-18 20:08:54 +0300

    vector class

--- script.js
+++ script.js
@@ -1,218 +1,112 @@
+let loglist = {}
+let ones =  {
+    add(name) {
+        ones[name] = 1
+    },
+    kill(name){
+        ones[name] = 0
+    }
+}
 const PI = Math.PI
 const canvas = document.querySelector("canvas")
 const cx = canvas.getContext("2d")
-canvas.width = 1200
-canvas.height = 720
 canvas.color = "black"
-let wn = 100
-let w1 = canvas.width / 20
-let wi = 0
-function drawCanvas() {
+canvas.width = 1200
+canvas.height = 800
+cx.fillRect(0, 0, canvas.width, canvas.height)
 
-    cx.fillStyle = canvas.color
-    cx.fillRect(0, 0, canvas.width, canvas.height)
-
+canvas.refresh = () => {
+    cx.fillStyle = canvas.color;
+    cx.fillRect(0, 0, canvas.width, canvas.height);
 }
-drawCanvas()
-let zeros = Array(10).fill(0)
-
-/* ---------- ----------> CANVAS CREATED <---------- ---------- */
-const el = {
-    vectorTable: document.querySelector(".vectorTable"),
-    velocityArrow: document.querySelector(".velocityArrow")
-}
-const anim = {
-    frame: { count: 0, rate: 60 },
-}
-const env = {
-    gForce: { x: 0, y: 10 },
-    friction: { coeffs: { air: 1, ground: 6 } },
-}
-
-class Cube {
-    mass = 1
-    forces = { user: { x: 0, y: 0 }, }
-    totalForce = { x: 0, y: 0 }
-    acclrtn = { x: 0, y: 0 }
-    velocity = { x: 0, y: 0 }
-    length = 20
-    width = this.length
-    height = this.length
-    get altitude() { return canvas.height - this.height - this.position.y }
-    position = { x: (canvas.width + this.width) / 2, y: (canvas.height + this.height) / 2 }
-
-    receiveForce(name, amount = { x: 0, y: 0 }) { this.forces[name] = amount }
-    renderVectorInfo() {
-        let vectorNames = ["totalForce", "acclrtn", "velocity", "position",]
-        let vectorMap = vectorNames.map((name) => {
-            return [name, {
-                x: document.querySelector(`.${name} .valueX`),
-                y: document.querySelector(`.${name} .valueY`),
-                arrowX: document.querySelector(`.${name} .arrowX`),
-                arrowY: document.querySelector(`.${name} .arrowY`),
-            }]
-        })
-        let vectorEl = Object.fromEntries(vectorMap)
-        for (const name of vectorNames) {
-            let el = vectorEl[name]
-            let value = this[name]
-
-            el.x.innerText = value.x.toFixed(1)
-            let yInversed = value.y * -1
-            el.y.innerText = yInversed.toFixed(1)
-            /* FIX-ME:  */
-            if (value.x > 0) {
-                el.x.classList.add("positive")
-                el.x.classList.remove("negative")
-                el.arrowX.classList.remove("otherDirection")
-            }
-            if (value.x < 0) {
-                el.x.classList.remove("positive")
-                el.x.classList.add("negative")
-                el.arrowX.classList.add("otherDirection")
-            }
-            if (value.x == 0) {
-                el.x.classList.remove("positive")
-                el.x.classList.remove("negative")
-            }
-
-            if (yInversed > 0) {
-                el.y.classList.add("positive")
-                el.y.classList.remove("negative")
-                el.arrowY.classList.remove("otherDirection")
-            }
-            if (yInversed < 0) {
-                el.y.classList.remove("positive")
-                el.y.classList.add("negative")
-                el.arrowY.classList.add("otherDirection")
-            }
-            if (yInversed == 0) {
-                el.y.classList.remove("positive")
-                el.y.classList.remove("negative")
-            }
-
+console.time("a")
+ones.add("a")
+class Vector {
+    static sum(vectors, name = "") {
+        let [x,y] = [0,0];
+        for (const vector of vectors) {
+            x += vector.x
+            y += vector.y
         }
+        return new Vector(x, y, name)
     }
+    constructor(x = 0, y = 0, name = "") {
+        if (name) { this.name = name; }
+        this.x = x;
+        this.y = y;
 
-    turnArrow() {
-        let { x, y } = this.velocity
-        y *= -1
-        let hip = Math.hypot(x, y)
-        let radian = Math.asin(y / hip)
-        let deg = (radian / PI) * 180
-        if (x < 0) { deg = 180 - deg }
-        el.velocityArrow.style.rotate = -deg + "deg"
-        el.velocityArrow.style.fontSize = hip + "px"
+
     }
+    get array() { return [this.x, this.y] }
+    get arrayNegY() { return [this.x, -this.y] }
+    get length() { return Math.hypot(this.x, this.y) }
 
-    constructor(color) { this.color = color }
 
-    get willOverflow() { return this.position.y + this.velocity.y + this.height > canvas.height }
-    land() {
-        this.position.y = canvas.height - this.height
-        this.velocity.y = 0
+    get angle() { return Math.atan2(this.y, this.x) }
+    get rad() { return this.angle / PI }
+    get deg() { return this.rad * 180 }
+
+    addInto(vector) {
+        this.x += vector.x
+        this.y += vector.y
     }
-    brake() {
-        this.velocity.y = this.velocity.x = this.acclrtn.x = this.acclrtn.y = 0
+    subtractInto(vector) {
+        this.x -= vector.x
+        this.y -= vector.y
     }
-    balance() {
-        let forces =  this.getTotalForces()
-        
-        let userForce = {x: forces.x / 2, y: forces.y / 2}
-
-        // let forces = this.forces
-        // for (const forceName in forces) {
-        //     const force = forces[forceName]
-        //     force.x = force.y = 0
-        // }
+    multiplyInto(num) {
+        this.x *= num
+        this.y *= num
     }
+    add(vector) { return new Vector(this.x + vector.x, this.y + vector.y) }
+    subtract(vector) { return new Vector(this.x - vector.x, this.y - vector.y) }
+    multiply(n) { return new Vector(this.x * n, this.y * n) }
+}
 
-    preventOverflow() {
-        let willOverflow = this.position.y + this.height + (this.velocity.y / anim.frame.rate) > canvas.height;
-        if (willOverflow) { this.land() };
-    }
-    counterForce() {
-        if (this.altitude == 0 && this.totalForce.y > 0) {
-            this.receiveForce("counter", { x: 0, y: -this.totalForce.y })
-        } else {
+const gForce = new Vector(0,-10)
+const canvasVector = new Vector(canvas.width, canvas.height)
+class Rectangle {
+    mass = 1;
+    position = new Vector();
+    size = new Vector(20, 20);
+    velocity = new Vector();
+    acceleration = new Vector();
+    forces = [gForce];
+    totalForceReceived = new Vector();
+    color = "#fafafa";
+    constructor() {
 
-            this.receiveForce("counter", { x: 0, y: 0 })
-        }
     }
     draw() {
-
-        drawCanvas()
-        cx.fillStyle = this.color
-        cx.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-    handleFriction() { let ground = this.forces.counter * env.friction.coeffs.ground }
-getTotalForces(){
-    let totalForce = { x: 0, y: 0 }
-    for (const force in this.forces) {
-        totalForce.x += this.forces[force].x
-        totalForce.y += this.forces[force].y
-    }
-    return totalForce
-}
-    setNewTotalForce() {
-        let totalForce = this.getTotalForces()
-        this.totalForce.x = totalForce.x
-        this.totalForce.y = totalForce.y
-    }
-    updatePhys() {
-        this.setNewTotalForce()
-        this.acclrtn.x = this.totalForce.x / this.mass
-        this.acclrtn.y = this.totalForce.y / this.mass
-        /* ----------  -------- ---------- ---------- ----------*/
-        this.velocity.x += this.acclrtn.x / anim.frame.rate
-        this.velocity.y += this.acclrtn.y / anim.frame.rate
-        this.position.x += this.velocity.x / anim.frame.rate
-        this.position.y += this.velocity.y / anim.frame.rate
-        this.preventOverflow()
-        this.counterForce()
+        cx.fillStyle = this.color;
+        cx.fillRect(this.position.x, canvas.height - this.position.y , this.size.x, -this.size.y);
     }
     update() {
-        this.draw()
-        this.updatePhys()
-        this.renderVectorInfo()
-        this.turnArrow()
+        this.totalForceReceived = Vector.sum(this.forces)
+        this.acceleration = this.totalForceReceived.multiply(1/this.mass)
+        this.velocity.addInto(this.acceleration.multiply(anim.frame.rate**-1))
+        this.position.addInto(this.velocity.multiply(anim.frame.rate**-1))
+
+        if(this.position.y <= 0 && ones.a){console.timeEnd("a"); ones.kill("a")}
+
+        if(this.position.y < 0){
+            this.position.y = 1;
+            this.velocity.addInto(this.velocity.multiply(-0.5))
+        }
     }
 }
 
-const cube = new Cube("green")
-cube.receiveForce("gravity", env.gForce)
+let r1 = new Rectangle()
+r1.position.addInto({x:300,y:125})
 
+const anim = { frame: { count: 0, rate: 60 }, }
 
 function animate() {
+    canvas.refresh()
     anim.frame.count++
     window.requestAnimationFrame(animate)
-    cube.update()
+    r1.update()
+    r1.draw()
 }
 
 animate()
-
-document.addEventListener("keydown", (e) => {
-    if (["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", 1, 2, 3, 4, "b", "f"].includes(e.key)) { e.preventDefault() }
-    console.log(e.key);
-
-    if (e.key == "a") { cube.forces.user.x-- }
-    if (e.key == "d") { cube.forces.user.x++ }
-    if (e.key == "w") { cube.forces.user.y-- }
-    if (e.key == "s") { cube.forces.user.y++ }
-
-    if (e.key == "ArrowLeft") { cube.velocity.x += -10 }
-    if (e.key == "ArrowRight") { cube.velocity.x += +10 }
-    if (e.key == "ArrowUp") { cube.velocity.y += -10 }
-    if (e.key == "ArrowDown") { cube.velocity.y += +10 }
-
-    if (e.key == "b") { cube.brake() }
-    if (e.key == "f") { cube.balance() }
-
-    if (e.key == "1") { console.time("timer1") }
-    if (e.key == "2") { console.timeEnd("timer1") }
-    if (e.key == "3") { console.time("timer2") }
-    if (e.key == "4") { console.timeEnd("timer2") }
-})
-
-// 2PI = 360deg
-// PI = 180deg
