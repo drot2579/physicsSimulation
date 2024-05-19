b8788d5b1c61d2756244f14a5056d866dc41da92
drot2579 <154900509+drotCode@users.noreply.github.com>
2024-05-19 15:27:32 +0300

    ...

--- script.js
+++ script.js
@@ -1,112 +1,111 @@
-let loglist = {}
-let ones =  {
-    add(name) {
-        ones[name] = 1
-    },
-    kill(name){
-        ones[name] = 0
-    }
-}
+let ones = { add(name) { ones[name] = 1 }, kill(name) { ones[name] = 0 } }
 const PI = Math.PI
-const canvas = document.querySelector("canvas")
-const cx = canvas.getContext("2d")
-canvas.color = "black"
-canvas.width = 1200
-canvas.height = 800
-cx.fillRect(0, 0, canvas.width, canvas.height)
-
-canvas.refresh = () => {
-    cx.fillStyle = canvas.color;
-    cx.fillRect(0, 0, canvas.width, canvas.height);
+let loglist = {}
+/* ---------- ---------- ---------- -------- ---------- ---------- ---------- */
+const canvas = document.querySelector("canvas");
+const cx = canvas.getContext("2d");
+let canvasWidth = 1200;
+let canvasHeight = 600;
+let canvasColor = "#f1f1f1"
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+let pxPm = 10;
+let canvasWidthMeter = canvasWidth / pxPm;
+let canvasHeightMeter = canvasHeight / pxPm;
+const draw = (color = "green", args = [0, 0, 1, 1]) => {
+    cx.fillStyle = color;
+    args.forEach((arg,idx) => args[idx] = arg * pxPm)
+    cx.fillRect(args[0], canvasHeight - args[1], args[2], -args[3])
 }
-console.time("a")
-ones.add("a")
+const refreshCanvas = () => draw(canvasColor, [0, 0, canvasWidth, canvasHeight])
 class Vector {
-    static sum(vectors, name = "") {
-        let [x,y] = [0,0];
-        for (const vector of vectors) {
-            x += vector.x
-            y += vector.y
-        }
-        return new Vector(x, y, name)
+    static sum(vectors) {
+        let [x, y] = [0, 0];
+        for (const vector of vectors) { x += vector.x; y += vector.y; }
+        return new Vector(x, y)
     }
-    constructor(x = 0, y = 0, name = "") {
-        if (name) { this.name = name; }
-        this.x = x;
-        this.y = y;
-
-
-    }
-    get array() { return [this.x, this.y] }
-    get arrayNegY() { return [this.x, -this.y] }
+    constructor(x = 0, y = 0) { this.x = x; this.y = y; }
     get length() { return Math.hypot(this.x, this.y) }
-
-
     get angle() { return Math.atan2(this.y, this.x) }
     get rad() { return this.angle / PI }
     get deg() { return this.rad * 180 }
 
-    addInto(vector) {
-        this.x += vector.x
-        this.y += vector.y
-    }
-    subtractInto(vector) {
-        this.x -= vector.x
-        this.y -= vector.y
-    }
-    multiplyInto(num) {
-        this.x *= num
-        this.y *= num
-    }
-    add(vector) { return new Vector(this.x + vector.x, this.y + vector.y) }
-    subtract(vector) { return new Vector(this.x - vector.x, this.y - vector.y) }
-    multiply(n) { return new Vector(this.x * n, this.y * n) }
+    get values() { return [this.x, this.y] }
+    get entries() { return { x: this.x, y: this.y } }
+
+    set(xy) { let [x, y] = Object.values(xy); this.x = x; this.y = y; }
+    add(xy) { let [x, y] = Object.values(xy); this.x += x; this.y += y; }
+    subtract(xy) { let [x, y] = Object.values(xy); this.x -= x; this.y -= y; }
+
+    addTo(xy) { let [x, y] = Object.values(xy); return new Vector(this.x + x, this.y + y) }
+    subtractTo(xy) { let [x, y] = Object.values(xy); return new Vector(this.x - x, this.y - y) }
+
+    multiply(num) { this.x *= num; this.y *= num; }
+    multiplyTo(n) { return new Vector(this.x * n, this.y * n) }
+    divide(num) { this.x /= num; this.y /= num; }
+    divideTo(n) { return new Vector(this.x / n, this.y / n) }
 }
 
-const gForce = new Vector(0,-10)
-const canvasVector = new Vector(canvas.width, canvas.height)
+let frameRate = 60;
+let gForce = new Vector(0, -10)
 class Rectangle {
-    mass = 1;
-    position = new Vector();
-    size = new Vector(20, 20);
-    velocity = new Vector();
-    acceleration = new Vector();
     forces = [gForce];
     totalForceReceived = new Vector();
-    color = "#fafafa";
-    constructor() {
-
+    acceleration = new Vector();
+    energy = {};
+    constructor( ) {
+        this.size =  new Vector(1, 1);
+        this.velocity =  new Vector(5, 25);
+        this.position =  new Vector(0, 10);
+        this.density =  1;
+        this.elasticity =  0.4;
+        this.color =  "#111";
+        this.volume = this.size.x * this.size.y;
+        this.mass = this.volume * this.density;
     }
     draw() {
-        cx.fillStyle = this.color;
-        cx.fillRect(this.position.x, canvas.height - this.position.y , this.size.x, -this.size.y);
+        draw(this.color, [...this.position.values, ...this.size.values])
     }
+    bounceX() { this.velocity.x *= -this.elasticity }
+    bounceY() { this.velocity.y *= -this.elasticity }
     update() {
+        this.draw()
+
         this.totalForceReceived = Vector.sum(this.forces)
-        this.acceleration = this.totalForceReceived.multiply(1/this.mass)
-        this.velocity.addInto(this.acceleration.multiply(anim.frame.rate**-1))
-        this.position.addInto(this.velocity.multiply(anim.frame.rate**-1))
+        this.acceleration = this.totalForceReceived.divideTo(this.mass)
+        this.velocity.add(this.acceleration.divideTo(frameRate))
+        this.position.add(this.velocity.divideTo(frameRate))
 
-        if(this.position.y <= 0 && ones.a){console.timeEnd("a"); ones.kill("a")}
-
-        if(this.position.y < 0){
-            this.position.y = 1;
-            this.velocity.addInto(this.velocity.multiply(-0.5))
+        if (this.position.y < 0) { this.position.y = 0; this.bounceY() }
+        if (this.position.x < 0) { this.position.x = 0; this.bounceX(); }
+        if (this.position.y + this.size.y > canvasHeightMeter) {
+            this.position.y = canvasHeightMeter - this.size.y; this.bounceY();
+        }
+        if (this.position.x + this.size.x > canvasWidth / pxPm) {
+            this.position.x = canvasWidth / pxPm - this.size.x; this.bounceX();
         }
     }
 }
 
-let r1 = new Rectangle()
-r1.position.addInto({x:300,y:125})
-
-const anim = { frame: { count: 0, rate: 60 }, }
+let r1 = new Rectangle([5,5]);
+let r2 = new Rectangle([10,10])
 
 function animate() {
-    canvas.refresh()
-    anim.frame.count++
+    refreshCanvas()
     window.requestAnimationFrame(animate)
     r1.update()
-    r1.draw()
+    r2.update()
 }
-
 animate()
+
+let keyVelocity = 10;
+document.body.addEventListener("keydown", (e) => {
+    console.log(e.key);
+    if (e.key == "a") { r1.velocity.add([-keyVelocity, 0]) }
+    if (e.key == "d") { r1.velocity.add([+keyVelocity, 0]) }
+    if (e.key == "s") { r1.velocity.add([0, -keyVelocity]) }
+    if (e.key == "w") { r1.velocity.add([0, +keyVelocity]) }
+    if (e.key == " ") { r1.velocity.set([0, 0]) }
+})
+
+
