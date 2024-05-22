e2f2559be9685c9aa8bd1769d129f6575cf2ae86
drot2579 <154900509+drotCode@users.noreply.github.com>
2024-05-22 19:41:12 +0300

    ...

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-let ones = { add(name) { ones[name] = 1 }, kill(name) { ones[name] = 0 } }
-const PI = Math.PI
-let loglist = {}
-/* ---------- ---------- ---------- -------- ---------- ---------- ---------- */
-const canvas = document.querySelector("canvas");
-const cx = canvas.getContext("2d");
-let canvasWidth = 1200;
-let canvasHeight = 600;
-let canvasColor = "#f1f1f1"
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
-let pxPm = 10;
-let canvasWidthMeter = canvasWidth / pxPm;
-let canvasHeightMeter = canvasHeight / pxPm;
-const draw = (color = "green", args = [0, 0, 1, 1]) => {
-    cx.fillStyle = color;
-    args.forEach((arg, idx) => args[idx] = arg * pxPm)
-    cx.fillRect(args[0], canvasHeight - args[1], args[2], -args[3])
+let logs = {}
+const bounceFn = (num, absolute, ratio) => {
+    let [value, sign] = [Math.abs(num), Math.sign(num)];
+    let newValue = value - Math.max(absolute, value * ratio);
+    return newValue < 0 ? 0 : newValue * -sign;
 }
-const refreshCanvas = () => draw(canvasColor, [0, 0, canvasWidth, canvasHeight])
+/* ---------- ---------- ---------- -------- ---------- ---------- ---------- */
+const canvasElement = document.querySelector("canvas");
+const cx = canvasElement.getContext("2d");
+canvasElement.width = 1200;
+canvasElement.height = 600;
+
+const canvas = {};
+canvas.pxPm = 50;
+canvas.color = "#f1f1f1"
+canvas.width = canvasElement.width / canvas.pxPm;
+canvas.height = canvasElement.height / canvas.pxPm;
+canvas.draw = (color = "green", args = [0, 0, 1, 1]) => {
+    cx.fillStyle = color;
+    let { pxPm, height } = canvas;
+    args = args.map(arg => arg * pxPm)
+    cx.fillRect(args[0], height * pxPm - args[1], args[2], -args[3])
+}
+canvas.refresh = () => canvas.draw(canvas.color, [0, 0, canvas.width, canvas.height])
+
 class Vector {
     static sum(vectors) {
         let [x, y] = [0, 0];
@@ -27,7 +32,7 @@ class Vector {
     constructor(x = 0, y = 0) { this.x = x; this.y = y; }
     get length() { return Math.hypot(this.x, this.y) }
     get angle() { return Math.atan2(this.y, this.x) }
-    get rad() { return this.angle / PI }
+    get rad() { return this.angle / Math.PI }
     get deg() { return this.rad * 180 }
 
     get values() { return [this.x, this.y] }
@@ -44,15 +49,30 @@ class Vector {
     multiplyTo(n) { return new Vector(this.x * n, this.y * n) }
     divide(num) { this.x /= num; this.y /= num; }
     divideTo(n) { return new Vector(this.x / n, this.y / n) }
+
+    fixedTo(n) { return new Vector((this.x).toFixed(n), (this.y).toFixed(n)) }
 }
 
 let frameRate = 60;
 let gForce = new Vector(0, -10)
+let fricCoef = {
+    air: -0.1,
+    surface: -0.4
+}
 class Rectangle {
-    forces = [gForce];
-    totalForceReceived = new Vector();
+    activeForces = {
+        gravity: gForce,
+        user: new Vector(),
+
+    };
+    passiveForces = {
+        counter: new Vector(),
+        surfaceFriction: new Vector(),
+        airFriction: new Vector(),
+
+    }
+    totalForce = new Vector();
     acceleration = new Vector();
-    energy = {};
     constructor() {
         this.size = new Vector(1, 1);
         this.velocity = new Vector(5, 25);
@@ -60,56 +80,167 @@ class Rectangle {
         this.density = 1;
         this.elasticity = 0.4;
         this.color = "#111";
-        this.volume = this.size.x * this.size.y;
-        this.mass = this.volume * this.density;
+        this.area = this.size.x * this.size.y;
+        this.mass = this.area * this.density;
     }
-    draw() {draw(this.color, [...this.position.values, ...this.size.values])}
-    bounceX() { this.velocity.x *= -this.elasticity }
-    bounceY() { this.velocity.y *= -this.elasticity }
-    canvasCollision() {
-        if (this.position.y < 0) { this.position.y = 0; this.bounceY() }
-        if (this.position.x < 0) { this.position.x = 0; this.bounceX(); }
-        if (this.position.y + this.size.y > canvasHeightMeter) {
-            this.position.y = canvasHeightMeter - this.size.y; this.bounceY();
+    draw() { canvas.draw(this.color, [...this.position.values, ...this.size.values]) }
+    stopX() { this.velocity.x = 0; }
+    stopY() { this.velocity.y = 0; }
+    stop() { this.velocity.set([0, 0]) }
+    bounceX() { this.velocity.x = bounceFn(this.velocity.x, 0.01, 1 - this.elasticity); }
+    bounceY() { this.velocity.y = bounceFn(this.velocity.y, 0.01, 1 - this.elasticity); }
+    calculate() {
+
+        //#region ---------- ---------- ----------* destructure *---------- ----------|
+        let { position, totalForce, velocity, size,
+            passiveForces: { counter, surfaceFriction, airFriction } } = this;
+
+        let leftBorder = position.x
+        let bottomBorder = position.y
+        let rightBorder = position.x + size.x
+        let topBorder = position.y + size.y
+
+        let leftTouch = leftBorder <= 0;
+        let bottomTouch = bottomBorder <= 0;
+        let rightTouch = rightBorder >= canvas.width;
+        let topTouch = topBorder >= canvas.height;
+
+        let leftSink = leftBorder < 0;
+        let bottomSink = bottomBorder < 0;
+        let rightSink = rightBorder > canvas.width;
+        let topSink = topBorder > canvas.height;
+
+        let rightLimit = canvas.width - size.x;
+        let topLimit = canvas.height - size.y;
+
+        let leftGoing = velocity.x < 0;
+        let rightGoing = velocity.x > 0;
+        let bottomGoing = velocity.y < 0;
+        let topGoing = velocity.y > 0;
+        //#endregion ---------- ---------- ----------* destructure *---------- ----------|
+
+
+        let startVelocity = this.velocity.multiplyTo(1)
+        let activeTotal = Vector.sum(Object.values(this.activeForces))
+
+        // counter force
+        this.passiveForces.counter.set([0, 0])
+        if (activeTotal.y < 0 && bottomTouch) { counter.y = -activeTotal.y };
+        if (activeTotal.y > 0 && topTouch) { counter.y = -activeTotal.y };
+        if (activeTotal.x < 0 && leftTouch) { counter.x = -activeTotal.x };
+        if (activeTotal.x > 0 && rightTouch) { counter.x = -activeTotal.x };
+
+
+        //  friction force
+        this.passiveForces.surfaceFriction.set([0, 0])
+        this.passiveForces.airFriction.set([0, 0])
+        let [surfaceFrictionX, surfaceFrictionY, airFrictionX, airFrictionY] = [0, 0, 0, 0]
+        if (velocity.x) {
+            if (counter.y) { surfaceFrictionX = Math.abs(counter.y) * Math.sign(velocity.x) * fricCoef.surface; }
+            airFrictionX = size.y * velocity.x * fricCoef.air;
         }
-        if (this.position.x + this.size.x > canvasWidth / pxPm) {
-            this.position.x = canvasWidth / pxPm - this.size.x; this.bounceX();
+        if (velocity.y) {
+            if (counter.x) { surfaceFrictionY = Math.abs(counter.x) * Math.sign(velocity.y) * fricCoef.surface; }
+            airFrictionY = size.x * velocity.y * fricCoef.air;
         }
-    }
-    mainCalculations() {
-        this.totalForceReceived = Vector.sum(this.forces)
-        this.acceleration = this.totalForceReceived.divideTo(this.mass)
-        this.velocity.add(this.acceleration.divideTo(frameRate))
+
+        this.passiveForces.surfaceFriction.set([surfaceFrictionX, surfaceFrictionY])
+        this.passiveForces.airFriction.set([airFrictionX, airFrictionY])
+
+        let passiveTotal = Vector.sum(Object.values(this.passiveForces))
+        let passiveAcceleration = passiveTotal.divideTo(this.mass)
+        let activeAcceleration = activeTotal.divideTo(this.mass)
+
+        this.velocity.add(activeAcceleration.divideTo(frameRate))
+        let testVelocity = this.velocity.addTo(passiveAcceleration.divideTo(frameRate))
+
+        if (testVelocity.x * this.velocity.x < 0) { testVelocity.x = 0 }
+        if (testVelocity.y * this.velocity.y < 0) { testVelocity.y = 0 }
+
+        this.acceleration = testVelocity.subtractTo(startVelocity).divideTo(1/frameRate)
+
+        this.totalForce = this.acceleration.multiplyTo(this.mass);
+        
+        this.velocity = testVelocity;
+
         this.position.add(this.velocity.divideTo(frameRate))
+
+        //      overflow correction
+        // position correction
+        if (leftSink) { position.x = 0; }
+        if (rightSink) { position.x = rightLimit; }
+        if (bottomSink) { position.y = 0; }
+        if (topSink) { position.y = topLimit; }
+
+        // velocity correction
+        if (leftTouch && leftGoing) { this.stopX() }
+        if (rightTouch && rightGoing) { this.stopX() }
+        if (bottomTouch && bottomGoing) { this.stopY() }
+        if (topTouch && topGoing) { this.stopY() }
+
+
     }
     update() {
-        this.mainCalculations()
+        this.calculate()
         this.draw()
-        this.canvasCollision()
+    }
+    renderInfo() {
+        let { position, velocity, acceleration, activeForces: { gravity, user },
+            passiveForces: { counter, airFriction, surfaceFriction }, totalForce } = this;
+        let tables = {
+            table1: { position, velocity, acceleration, },
+            table2: { user, gravity, counter, airFriction, surfaceFriction, totalForce },
+        }
+
+        for (const tableName in tables) {
+            const table = tables[tableName]
+
+            for (const propName in table) {
+                const { x, y } = table[propName].fixedTo(1);
+                let xEl = document.querySelector(`.${tableName}>.row.${propName}>.x`)
+                let yEl = document.querySelector(`.${tableName}>.row.${propName}>.y`)
+                xEl.innerText = x;
+                xEl.classList.remove("pos", "neg")
+                if (x > 0) { xEl.classList.add("pos") }
+                if (x < 0) { xEl.classList.add("neg") }
+                yEl.innerText = y;
+                yEl.classList.remove("pos", "neg")
+                if (y > 0) { yEl.classList.add("pos") }
+                if (y < 0) { yEl.classList.add("neg") }
+            }
+        }
+
+
     }
 }
 
 let r1 = new Rectangle();
-let r2 = new Rectangle()
-r2.color = "#2020aa";
-r2.size.set([3, 5])
 
+let framesCount = 0;
+let frameLimit = 0;
 function animate() {
-    refreshCanvas()
-    window.requestAnimationFrame(animate)
+    canvas.refresh()
     r1.update()
-    r2.update()
+    r1.renderInfo()
+    if (frameLimit && framesCount > frameLimit) { return }
+    window.requestAnimationFrame(animate)
 }
 animate()
 
-let keyVelocity = 10;
+//#region ---------- ---------- ----------* keys *---------- ----------|
+let keyValue = 15;
 document.body.addEventListener("keydown", (e) => {
-    console.log(e.key);
-    if (e.key == "a") { r1.velocity.add([-keyVelocity, 0]) }
-    if (e.key == "d") { r1.velocity.add([+keyVelocity, 0]) }
-    if (e.key == "s") { r1.velocity.add([0, -keyVelocity]) }
-    if (e.key == "w") { r1.velocity.add([0, +keyVelocity]) }
-    if (e.key == " ") { r1.velocity.set([0, 0]) }
+    if (e.key == " ") { r1.stop() }
+    if (e.key == "a") { r1.activeForces.user.x = -keyValue }
+    if (e.key == "d") { r1.activeForces.user.x = +keyValue }
+    if (e.key == "s") { r1.activeForces.user.y = -keyValue }
+    if (e.key == "w") { r1.activeForces.user.y = +keyValue }
 })
-
+document.body.addEventListener("keyup", (e) => {
+    if (e.key == "a") { r1.activeForces.user.x = 0 }
+    if (e.key == "d") { r1.activeForces.user.x = 0 }
+    if (e.key == "s") { r1.activeForces.user.y = 0 }
+    if (e.key == "w") { r1.activeForces.user.y = 0 }
+})
+//#endregion ---------- ---------- ----------* keys *---------- ----------|
 
